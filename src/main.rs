4828fd8cd3b0133407b4225ca4227c6befac6cd5
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-21 10:25:58 -0500

    menu screen and loop

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,101 @@ enum GameMode {
     End,
 }
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State{
+    fn new() -> Self {
+        State{
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm){
+        // TODO: Fill in this stub later
+        self.mode = GameMode::End;
+    }
+    
+    fn restart(&mut self){
+        self.mode = GameMode::Playing;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "You died!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key{
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _=>{}
+            }
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon!");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _=>{}
+            }
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal! {frames}" );
+        match self.mode{
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
+    }
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm){
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437(@)
+        );
+    }
+
+    fn gravity_and_move(&mut self){
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
     }
 }
 
@@ -20,5 +109,5 @@ fn main() -> BError {
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
