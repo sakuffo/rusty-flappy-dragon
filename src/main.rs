f3cca9af2fbd57b726f63a238dde3c175482628d
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-21 10:46:35 -0500

    got the dragon @ on the screen

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,61 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
-impl State{
+impl State {
     fn new() -> Self {
-        State{
+        State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
-    fn play(&mut self, ctx: &mut BTerm){
-        // TODO: Fill in this stub later
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap!");
     }
-    
-    fn restart(&mut self){
+
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
-    fn dead(&mut self, ctx: &mut BTerm){
+    fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You died!");
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
-        if let Some(key) = ctx.key{
+        if let Some(key) = ctx.key {
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _=>{}
+                _ => {}
             }
         }
     }
 
-    fn main_menu(&mut self, ctx: &mut BTerm){
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "Welcome to Flappy Dragon!");
         ctx.print_centered(8, "(P) Play Game");
@@ -51,7 +70,7 @@ impl State{
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _=>{}
+                _ => {}
             }
         }
     }
@@ -59,7 +78,7 @@ impl State{
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        match self.mode{
+        match self.mode {
             GameMode::Menu => self.main_menu(ctx),
             GameMode::Playing => self.play(ctx),
             GameMode::End => self.dead(ctx),
@@ -74,7 +93,7 @@ struct Player {
 }
 
 impl Player {
-    fn new(x: i32, y: i32) -> {
+    fn new(x: i32, y: i32) -> Self {
         Player {
             x,
             y,
@@ -82,17 +101,11 @@ impl Player {
         }
     }
 
-    fn render(&mut self, ctx: &mut BTerm){
-        ctx.set(
-            0,
-            self.y,
-            YELLOW,
-            BLACK,
-            to_cp437(@)
-        );
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
     }
 
-    fn gravity_and_move(&mut self){
+    fn gravity_and_move(&mut self) {
         if self.velocity < 2.0 {
             self.velocity += 0.2;
         }
@@ -101,6 +114,10 @@ impl Player {
         if self.y < 0 {
             self.y = 0;
         }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
     }
 }
 
