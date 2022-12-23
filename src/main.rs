6d3a2c38623792fad249699ea37f9bb2ad1f20d4
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-23 05:35:49 -0500

    score after dead

--- src/main.rs
+++ src/main.rs
@@ -65,6 +65,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You died!");
+        ctx.print_centered(6, &format!("Score: {}", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
